5ce9625bd37cf76c174d35332f63bff31e21f287
Jansen Rodriguez <JansenR125@gmail.com>
2021-02-02 15:42:50 -0500

    Added controller and settings for customized login form

--- src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
+++ src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
@@ -2,6 +2,7 @@ package com.luv2code.springsecurity.demo.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -21,6 +22,18 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 			.withUser(users.username("john").password("test123").roles("EMPLOYEE"))
 			.withUser(users.username("mary").password("test123").roles("MANAGER"))
 			.withUser(users.username("susan").password("test123").roles("ADMIN"));
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http.authorizeRequests()
+				.anyRequest().authenticated() // Any request coming in must be authenticated
+			.and()
+			.formLogin() // Customize the login form
+				.loginPage("/showMyLoginPage") // Mapping for login page
+				.loginProcessingUrl("/authenticateTheUser") // Where the user login form gets processed
+				.permitAll(); // Allows everyone to see the login page (you dont have to be logged in to see it)
 	}
 	
 }
